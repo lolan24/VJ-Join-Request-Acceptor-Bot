dcfdab9779643f1112d141427e068b2e7731a93c
lolan24 <asianetbot@gmail.com>
2025-04-05 02:06:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,4 +13,4 @@ DB_URI = environ.get("DB_URI", "mongodb+srv://join:join@cluster0.ojdfgdu.mongodb
 DB_NAME = environ.get("DB_NAME", "join")
 
 # If this is True Then Bot Accept New Join Request 
-NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', False))
+NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True))
